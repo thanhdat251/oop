aba6104f60ffd8f38be924e08e54bc530a38eec6
your name <abc@gmail.com>
2021-11-24 22:40:22 +0700

    new

---
+++ NPL.SMS/R2S.Training.DAO/LineItemDAO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NPL.SMS.R2S.Training.DAO
+{
+    class LineItemDAO
+    {
+    }
+}
---
+++ NPL.SMS/R2S.Training.Entities/Customer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NPL.SMS.R2S.Training.Entities
+{
+    class Customer
+    {
+        private int customerId;
+        private string customerName;
+
+        public int CustomerId { get => customerId; set => customerId = value; }
+        public string CustomerName { get => customerName; set => customerName = value; }
+
+        public Customer()
+        {
+
+        }
+
+        public Customer(int customerId, string customerName)
+        {
+            CustomerId = customerId;
+            CustomerName = customerName;
+        }     
+    }
+}
